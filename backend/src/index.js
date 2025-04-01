12bbeff1ed6b6f3e42c9afe1aea97b2347e1b471
karthik <karthikvempala31@gmail.com>
2025-04-01 17:06:54 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { createServer } from "http";
+
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
