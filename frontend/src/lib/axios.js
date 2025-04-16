7ddbd0b490515e38bb240ac3f3ebb4e50cecabd1
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-04-16 23:01:22 +0530

    fix:added production url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE==="development" ? "http://localhost:5003/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5003/api"
+      : "https://chat-app-full-1-k206.onrender.com",
   withCredentials: true,
 });
